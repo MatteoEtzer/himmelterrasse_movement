0d098947cd4d29731ae34343874a43e674aeffc4
MatteoEtzer <matteoetzer@gmx.at>
2020-11-30 17:10:00 +0100

    randomize is in there

---
+++ Assets/Scripts/randomizeCubes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomizeCubes : MonoBehaviour
+{
+
+    public GameObject CubePrefab;
+
+    public Vector3 center;
+    public Vector3 size;
+    public int count;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            for (int i = 0; i < count; i++)
+            {   
+                spawnCube();
+            }
+        }
+    }
+
+    
+    public void spawnCube()
+    {
+        Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x /2), Random.Range(-size.y / 2, size.y /2), Random.Range(-size.z / 2, size.z /2));
+
+        Instantiate(CubePrefab, pos, Quaternion.identity); 
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1,0,0,0.5f);
+        Gizmos.DrawCube(center, size);
+    }
+}
