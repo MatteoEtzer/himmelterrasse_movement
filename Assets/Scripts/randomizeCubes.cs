9be7a15866f1c09a502645e25b04292fd90151d7
aleksaradic <aleksaradic01@gmail.com>
2020-12-14 19:10:06 +0100

    Randomize Script angepasst. OnClick werden Cubes gelöscht und automatisch neue erstellt

--- Assets/Scripts/randomizeCubes.cs
+++ Assets/Scripts/randomizeCubes.cs
@@ -10,25 +10,53 @@ public class randomizeCubes : MonoBehaviour
     public Vector3 center;
     public Vector3 size;
     public int count;
+    public GameObject[] listGameObjects;
+
+    void Start()
+    {
+        SpawnCube();
+    }
 
     void Update()
     {
+        /*
         if(Input.GetKeyDown(KeyCode.Q))
         {
-                spawnCube();
+            DestroyObjects();
+            spawnCube();
         }
+        */
     }
 
     
-    public void spawnCube()
+    public void SpawnCube()
     {
+        listGameObjects = new GameObject[count];
+
         for (int i = 0; i < count; i++)
         {
             Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x /2), Random.Range(-size.y / 2, size.y /2), Random.Range(-size.z / 2, size.z /2));
 
-            Instantiate(CubePrefab, pos, Quaternion.identity); 
+            GameObject clone = Instantiate(CubePrefab, pos, Quaternion.identity);
+            
+            listGameObjects[i] = clone;
         }
     }
+
+    void DestroyObjects()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Destroy(listGameObjects[i]);
+        }
+    }
+
+    public void DoAll()
+    {
+        DestroyObjects();
+        SpawnCube();
+    }
+
     void OnDrawGizmosSelected()
     {
         Gizmos.color = new Color(1,0,0,0.5f);
---
+++ Assets/_nstantiate.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiate : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject[] listGameObjects;
+    public int numberCopies = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numberCopies; i++)
+        {
+            Destroy(listGameObjects[i]);
+        }
+
+        listGameObjects = new GameObject[numberCopies];
+
+        for (int i = 0; i < numberCopies; i++)
+        {
+           GameObject clone = Instantiate(prefab);
+            listGameObjects[i] = clone;
+        }
+
+        
+    }
+
+
+
+    void DestroyObjects()
+    {
+        for (int i = 0; i < numberCopies; i++)
+        {
+            Destroy(listGameObjects[i]);
+        }
+    }
+}
